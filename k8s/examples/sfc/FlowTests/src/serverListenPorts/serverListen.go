36c94329e1a96437d7b7444568e05e99564e1d49
Adel Bouridah <a.bouridah@centre-univ-mila.dz>
2020-01-07 10:27:44 +0100

    Flows Tests Simulation-ALLDirectory:GENERATED FLOW IN Priority Queue
    Signed-off-by: Adel Bouridah <a.bouridah@centre-univ-mila.dz>

---
+++ k8s/examples/sfc/FlowTests/src/serverListenPorts/serverListen.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	//"ketama"
+	"strconv"
+	"os/exec"
+	"strings"
+	"log"
+	"os"
+	//"io"
+	//"encoding/json"
+	//"sync"
+	//"bytes"
+)
+func main() {
+	fmt.Println(" *************************************************************************")
+	fmt.Println(" ******			Get Output Paths CNFs		     **********")
+	fmt.Println(" *************************************************************************")
+	// 2. get Output paths CNF - Pods names  (to execute later iperf on these cnfs)
+	cmd2get := exec.Command("kubectl", "get", "pods", )
+	cmd2grep := exec.Command("grep", "-oE","rep-linux-cnf4\\S*")
+
+	// Get cmdget's stdout and attach it to grep's stdin.
+	pipe2, _ := cmd2get.StdoutPipe()
+	defer pipe2.Close()
+
+	cmd2grep.Stdin = pipe2
+
+	// Run cmdget first.
+	cmd2get.Start()
+
+	// Run and get the output of grep.
+	res2, _ := cmd2grep.Output()
+	a2:=string(res2)
+	outputsCNFs := strings.Split(a2,"\n")
+	//outputsCNFs contain all the inputs CNFs, whereas the last element is a whitespace one because of the '\n'
+	//tests
+	for i := 0; i <= len(outputsCNFs)-2; i++ {
+		fmt.Println("cnf outputs	", outputsCNFs[i])
+	}
+	fmt.Println(" **************************************************************************************************")
+	fmt.Println(" ******   Make output iperf servers listen on sevral Ports -For parallel Flows test      **********")
+	fmt.Println(" **************************************************************************************************")
+	//var int endPorts
+	beginPorts, _ := strconv.Atoi(os.Args[1])
+	endPorts, _ := strconv.Atoi(os.Args[2])
+	fmt.Println(" end ports : "+strconv.Itoa(endPorts))
+	for i := 0; i <= len(outputsCNFs)-2; i++ {
+		if (endPorts<=9){
+			for j := beginPorts; j <= endPorts; j++ {
+				//fmt.Println("cnf outputs	", outputsCNFs[i])
+				cmd := exec.Command("kubectl", "exec", outputsCNFs[i] , "--","iperf", "-s", "-p", "500"+strconv.Itoa(j))
+				//cmd := exec.Command("kubectl", "get", "pods", )
+				err := cmd.Start()
+				if err != nil {
+					log.Fatalf("cmd.Run() failed with %s\n", err)
+				}
+				//fmt.Printf("combined out:\n%s\n", string(out))
+				fmt.Println("CNF listen on  port  ", outputsCNFs[i] , " on port 500"+strconv.Itoa(j))
+			}
+		}else{
+			if (beginPorts<9){
+					for j := beginPorts; j <= 9; j++ {
+						//fmt.Println("cnf outputs	", outputsCNFs[i])
+						cmd := exec.Command("kubectl", "exec", outputsCNFs[i] , "--","iperf", "-s", "-p", "500"+strconv.Itoa(j))
+						//cmd := exec.Command("kubectl", "get", "pods", )
+						err := cmd.Start()
+						if err != nil {
+							log.Fatalf("cmd.Run() failed with %s\n", err)
+						}
+						//fmt.Printf("combined out:\n%s\n", string(out))
+						fmt.Println("CNF listen on  port   ", outputsCNFs[i] , " on port 500"+strconv.Itoa(j))
+					}
+					beginPorts=10; //to continu from 10 ports in the next spet
+			}
+			for j := beginPorts; j <= endPorts; j++ {
+				//fmt.Println("cnf outputs	", outputsCNFs[i])
+				cmd := exec.Command("kubectl", "exec", outputsCNFs[i] , "--","iperf", "-s", "-p", "50"+strconv.Itoa(j), "-u")
+				//cmd := exec.Command("kubectl", "get", "pods", )
+				err := cmd.Start()
+				if err != nil {
+					log.Fatalf("cmd.Run() failed with %s\n", err)
+				}
+				//fmt.Printf("combined out:\n%s\n", string(out))
+				fmt.Println("CNF listen on  port  ", outputsCNFs[i] , " on port 50"+strconv.Itoa(j))
+			}
+
+		}
+
+	}
+
+}
